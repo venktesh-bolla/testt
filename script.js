f6f73d69564ce575f4307f43056c59ba8b2f9b40
venktesh <venktesh.bolla@gmail.com>
2023-05-29 17:03:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,74 +1,16 @@
-// Smooth scrolling
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
+// Scroll reveal
+window.addEventListener('scroll', reveal);
 
-// Reveal animations
-const sections = document.querySelectorAll('section');
+function reveal() {
+  var reveals = document.querySelectorAll('.scroll-reveal');
 
-const revealSection = () => {
-    sections.forEach(section => {
-        const sectionTop = section.getBoundingClientRect().top;
-        const windowHeight = window.innerHeight;
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var revealTop = reveals[i].getBoundingClientRect().top;
+    var revealPoint = 150;
 
-        if (sectionTop < windowHeight - 100) {
-            section.classList.add('reveal');
-        } else {
-            section.classList.remove('reveal');
-        }
-    });
-};
-
-window.addEventListener('scroll', revealSection);
-
-// Parallax effect
-window.addEventListener('scroll', () => {
-    const parallaxElements = document.querySelectorAll('.parallax');
-
-    parallaxElements.forEach(element => {
-        const distanceFromTop = element.getBoundingClientRect().top;
-        const elementHeight = element.offsetHeight;
-        const windowHeight = window.innerHeight;
-        const speed = element.getAttribute('data-parallax-speed');
-
-        if (distanceFromTop < windowHeight && distanceFromTop + elementHeight > 0) {
-            element.style.transform = `translateY(${distanceFromTop * speed}px)`;
-        }
-    });
-});
-
-// Slideshow
-const slides = document.querySelectorAll('.slideshow-slide');
-const slideInterval = 3000; // Interval between slides in milliseconds
-let currentSlide = 0;
-
-const nextSlide = () => {
-    slides[currentSlide].classList.remove('active');
-    currentSlide = (currentSlide + 1) % slides.length;
-    slides[currentSlide].classList.add('active');
-};
-
-setInterval(nextSlide, slideInterval);
-
-// Scroll reveal for individual elements
-const scrollRevealElements = document.querySelectorAll('.scroll-reveal');
-
-const scrollReveal = () => {
-    scrollRevealElements.forEach(element => {
-        const distanceFromTop = element.getBoundingClientRect().top;
-        const windowHeight = window.innerHeight;
-
-        if (distanceFromTop < windowHeight - 100) {
-            element.classList.add('reveal');
-        } else {
-            element.classList.remove('reveal');
-        }
-    });
-};
-
-window.addEventListener('scroll', scrollReveal);
+    if (revealTop < windowHeight - revealPoint) {
+      reveals[i].classList.add('reveal');
+    }
+  }
+}
