79537e6f6e25148baa16cf22a9a69b32a3c5ed42
venktesh <venktesh.bolla@gmail.com>
2023-06-03 15:18:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 /*Enter your js code here*/
+function navigate(direction) {
+  // Handle navigation logic here
+  if (direction === 'left') {
+    // Perform left navigation
+  } else if (direction === 'right') {
+    // Perform right navigation
+  }
+}
