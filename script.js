b3dccd182bc057cce0a0b7b8b2d6b8e2a6e0e60b
venktesh <venktesh.bolla@gmail.com>
2023-06-03 14:10:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
-// Scroll reveal
-window.addEventListener('scroll', reveal);
-
-function reveal() {
-  var reveals = document.querySelectorAll('.scroll-reveal');
-
-  for (var i = 0; i < reveals.length; i++) {
-    var windowHeight = window.innerHeight;
-    var revealTop = reveals[i].getBoundingClientRect().top;
-    var revealPoint = 150;
-
-    if (revealTop < windowHeight - revealPoint) {
-      reveals[i].classList.add('reveal');
-    }
-  }
-}
+/*Enter your js code here*/
